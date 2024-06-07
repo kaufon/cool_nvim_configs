9bce3f24bf52410eed6668d1bcb15875d25db3b8
kaufon <kauanfvale@gmail.com>
2024-06-07 14:46:31 -0300

    add lsp for html dockercompose and dockerfile | fix lsp servers name

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,12 +4,15 @@ local capabilities = base.capabilities
 local lspconfig = require("lspconfig")
 
 local servers = {
+  -- lsp server name goes here,for consult go to: https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc%2Fserver-mapping.md
   "clangd",
   "pyright",
   "solargraph",
-  "lua-language-server",
-  "html-lsp",
-  "htmx-lsp"
+  "lua_ls",
+  "html",
+  "docker_compose_language_service", -- this one is just done by microsoft so it is REALLY shitty
+  "dockerls",
+  --"htmx" is a work in progress to see progress go to: https://github.com/ThePrimeagen/htmx-lsp  | maybe in near future will work!
 
 }
 for _, server_name in ipairs(servers) do
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -11,13 +11,15 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        --mason package name goes here,for consult go to: https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc%2Fserver-mapping.md
         "clangd",
         "pyright",
         "solargraph",
         "lua-language-server",
-        "html-lsp",
-        "htmx-lsp"
-
+     -- "html-lsp",
+        "htmx-lsp",
+        "docker-compose-language-service",
+        "dockerfile-language-server"
       }
     }
   },
