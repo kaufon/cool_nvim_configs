de5e7d4275de27db5930dd01f19579ec5327de5c
kaufon <kauanfvale@gmail.com>
2024-06-09 12:46:41 -0300

    fix autoformat, close #1

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -2,20 +2,20 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting",{})
 
 local opts = {
-  source ={
+  sources ={
     null_ls.builtins.formatting.prettierd,
   },
   on_attach = function (client,bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({
-        group = group,
+        group = augroup,
         buffer = bufnr
       })
       vim.api.nvim_create_autocmd("BufWritePre",{
         group = augroup,
         buffer = bufnr,
         callback = function ()
-          
+          vim.lsp.buf.format({bufnr = bufnr})
         end,
       })
     end
