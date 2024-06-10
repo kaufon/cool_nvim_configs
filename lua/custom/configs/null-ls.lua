b6dce1f31ddd6e6ccceb21732246ecb590ac9aee
kaufon <kauanfvale@gmail.com>
2024-06-09 23:30:27 -0300

    comment autoformat on save

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -10,21 +10,21 @@ local opts = {
     null_ls.builtins.diagnostics.solhint,
   },
 
-  on_attach = function (client,bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
-        group = augroup,
-        buffer = bufnr
-      })
-      vim.api.nvim_create_autocmd("BufWritePre",{
-        group = augroup,
-        buffer = bufnr,
-        callback = function ()
-          vim.lsp.buf.format({bufnr = bufnr})
-        end,
-      })
-    end
-  end
+--  on_attach = function (client,bufnr)
+  --  if client.supports_method("textDocument/formatting") then
+    --  vim.api.nvim_clear_autocmds({
+      --  group = augroup,
+        --buffer = bufnr
+      --})
+      --vim.api.nvim_create_autocmd("BufWritePre",{
+        --group = augroup,
+        --buffer = bufnr,
+        --callback = function ()
+          --vim.lsp.buf.format({bufnr = bufnr})
+        --end,
+      --})
+    --end
+ -- end
 }
 
 return opts
