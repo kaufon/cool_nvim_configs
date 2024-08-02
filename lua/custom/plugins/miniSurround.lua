85f1fc70d8165aed20e6ea4f7bab13d08e0847e4
kaufon <kauanfvale@gmail.com>
2024-08-02 17:55:56 -0300

    refactor minisurround

---
+++ lua/custom/plugins/miniSurround.lua
@@ -0,0 +1,19 @@
+local miniSurround = {
+
+
+
+  'echasnovski/mini.surround',
+
+  event = "VeryLazy",
+
+  version = false,
+
+  config = function()
+    require("mini.surround").setup()
+  end
+
+
+
+}
+
+return miniSurround
