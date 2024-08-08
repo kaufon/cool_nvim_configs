0c9d2f0be53ee9e65f162300cb67f0d0c11afbb0
joelberto <kauanfvale@gmail.com>
2024-08-08 08:36:23 -0300

    disabled autosave msg

--- lua/custom/plugins/autoSave.lua
+++ lua/custom/plugins/autoSave.lua
@@ -6,6 +6,15 @@ local autoSave =
   event = { "InsertLeave", "TextChanged" },
   opts = {
 
+    enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    execution_message = {
+      enabled = false,
+      message = function() -- message to print on save
+        return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+      end,
+      dim = 0.18,             -- dim the color of `message`
+      cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    },
   },
 }
 return autoSave
