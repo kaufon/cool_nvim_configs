b3717c5fdc378d4bcac3a22240aeea06c15e5817
kaufon <kauanfvale@gmail.com>
2024-08-16 21:04:40 -0300

    add codem

---
+++ lua/custom/plugins/codeium.lua
@@ -0,0 +1,21 @@
+local codeium = {"Exafunction/codeium.vim",
+		event = "InsertEnter",
+		config = function()
+			vim.g.codeium_disable_bindings = 1
+			vim.g.codeium_filetypes = { TelescopePrompt = false }
+
+			vim.keymap.set("i", "<C-j>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+			vim.keymap.set("i", "<C-f>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true })
+			vim.keymap.set("i", "<C-b>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true })
+			vim.keymap.set("i", "<C-]>", function()
+				return vim.fn["codeium#Clear"]()
+			end, { expr = true })
+		end,
+	}
+return codeium
