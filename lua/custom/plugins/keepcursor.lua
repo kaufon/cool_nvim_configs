14d852cfa48e248fc1bc0623f1924f89ee8c0f3d
kaufon <kauanfvale@gmail.com>
2024-10-04 10:25:38 -0300

    keep cursor added

---
+++ lua/custom/plugins/keepcursor.lua
@@ -0,0 +1,9 @@
+local keepCursor = {
+  "rlychrisg/keepcursor.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled_on_start_v = "middle",
+    enabled_on_start_h = "none"
+  }
+}
+return keepCursor
