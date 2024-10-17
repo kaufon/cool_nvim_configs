a6450b5ee69cb009e525a970f4fe1955405b5ba1
kaufon <kauanfvale@gmail.com>
2024-10-17 08:17:16 -0300

    suff

--- lua/custom/plugins/leapNvim.lua
+++ lua/custom/plugins/leapNvim.lua
@@ -2,9 +2,13 @@ return {
   "ggandor/leap.nvim",
   init = function()
     require('leap')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>(leap-backward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'fg', '<Plug>(leap-from-window)')
+
+    vim.keymap.set('n', 'f', '<Plug>(leap)')
+    vim.keymap.set('n', 'F', '<Plug>(leap-from-window)')
+    vim.keymap.set({ 'x', 'o' }, 'f', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'x', 'o' }, 'F', '<Plug>(leap-backward)')
+    require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
   end,
   dependencies = {
     "tpope/vim-repeat"
