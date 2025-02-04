a5eddf8e3886d2a167e1b6996a2bee2967323dc6
kaufon <kauanfvale@gmail.com>
2025-02-04 08:27:26 -0300

    add neotest with Junit

--- lua/custom/plugins/neoTest.lua
+++ lua/custom/plugins/neoTest.lua
@@ -1,21 +1,26 @@
+local f = require('utils.file').is_pkg_dir
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "olimorris/neotest-rspec",
-    "marilari88/neotest-vitest",
-    "rcasia/neotest-java",
-  },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-rspec"),
-        require("neotest-vitest"),
-        require("neotest-java")
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "olimorris/neotest-rspec",
+      "marilari88/neotest-vitest",
+      {
+        'nvim-java/neotest-jdtls',
       },
-    })
-  end
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-rspec"),
+          require("neotest-vitest"),
+          require('neotest-jdtls')
+        },
+      })
+    end,
+  },
 }
