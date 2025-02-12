54bf6484db8a79c6a2f3df2e829e7f144c781b2a
kaufon <kauanfvale@gmail.com>
2025-02-12 15:49:52 -0300

    make jdtls handle all java keymaps

--- lua/custom/plugins/Jdtls.lua
+++ lua/custom/plugins/Jdtls.lua
@@ -19,10 +19,26 @@ return {
     },
   },
   config = function()
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'LSP declaration' })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'LSP definition' })
+    vim.keymap.set('n', 'gr', function()
+      require("telescope.builtin").lsp_references()
+    end, { desc = 'LSP references' })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'LSP implementation' })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'LSP hoveer' })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'LSP rename' })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP code action' })
+    vim.keymap.set('n', '<leader>ds', vim.lsp.buf.document_symbol, { desc = 'Document Symbols' })
+    vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, { desc = 'Workspace Symbols' })
+    vim.keymap.set('n', '<leader>sd', vim.diagnostic.open_float, { desc = 'Show Diagnostics' })
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous Diagnostic' })
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next Diagnostic' })
+
     vim.api.nvim_set_keymap('n', '<leader>crc', ':JavaRefactorExtractConstant<CR>', { desc = "Extract Constant" })
     vim.api.nvim_set_keymap('n', '<leader>crv', ':JavaRefactorExtractVariable<CR>', { desc = "Extract Variable" })
     vim.api.nvim_set_keymap('n', '<leader>crm', ':JavaRefactorExtractMethod<CR>', { desc = "Extract Method" })
     vim.api.nvim_set_keymap('n', '<leader>crf', ':JavaRefactorExtractField<CR>', { desc = "Extract Field" })
-    vim.api.nvim_set_keymap('n', '<leader>cra', ':JavaRefactorExtractVariableAllOccurrence<CR>',{ desc = "Extract All occurrences of value" })
+    vim.api.nvim_set_keymap('n', '<leader>cra', ':JavaRefactorExtractVariableAllOccurrence<CR>',
+      { desc = "Extract All occurrences of value" })
   end,
 }
